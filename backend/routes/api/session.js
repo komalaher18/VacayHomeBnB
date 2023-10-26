95b6544d68ffd0a83a7283103fd45c6104a43744
Komal aher <komalaher18@gmail.com>
2023-10-26 12:07:40 -0700

    minor changes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -9,14 +9,16 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Log in
+
 const validateLogin = [
   check('credential')
     .exists({ checkFalsy: true })
     .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
+    .withMessage('Email or username is required'),
   check('password')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
+    .withMessage('Password is required'),
   handleValidationErrors
 ];
 
@@ -36,11 +38,12 @@ router.post(
     });
 
     if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = { credential: 'The provided credentials were invalid.' };
-      return next(err);
+      // const err = new Error('Invalid credentials');
+      // err.status = 401;
+      // err.title = 'Login failed';
+      // err.errors = { "message": 'Invalid credentials' };
+      // return next(err);
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     const safeUser = {
