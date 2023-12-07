825198fa7088d45b3c52d24e00485b40e70daca6
Komal aher <komalaher18@gmail.com>
2023-12-06 22:58:07 -0800

    completed frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore() ;
 
@@ -11,6 +12,12 @@ if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
 
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
