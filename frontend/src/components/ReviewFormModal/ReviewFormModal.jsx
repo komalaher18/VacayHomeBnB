9f355e679bedef59ec0ba1128c124510baef11a9
Komal aher <komalaher18@gmail.com>
2023-12-23 03:06:13 -0800

    error correction in reviewform modal

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -1,103 +1,73 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
-import { addReviewToSpot } from "../../store/spots";
 import "./ReviewFormModal.css";
 import { useModal } from "../../context/Modal";
+import * as reviewsActions from "../../store/spots";
 
 const ReviewFormModal = ({ spotId, spotFetch }) => {
-  const [review, setReview] = useState("");
-  const [stars, setStars] = useState(0);
-  const [error, setError] = useState("");
-  const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
+  const [review, setReview] = useState("");
+  const [stars, setStars] = useState("");
+  const [errors, setErrors] = useState({});
+  const [starCount, setStarCount] = useState(null);
   const { closeModal } = useModal();
+  const sessionUser = useSelector((state) => state.session.user);
 
-  const handleSubmit = () => {
-    setError();
-    dispatch(addReviewToSpot(spotId, review, stars, sessionUser))
-      .then(() => {
-        spotFetch();
-        closeModal();
-      })
-      .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.message) {
-          setError(data.message);
-        }
-      });
+  const starSelect = () => {
+    setStars(starCount);
+  };
+
+  const starHovering = (num) => {
+    setStarCount(num);
+  };
+
+  let fiveStars = [1, 2, 3, 4, 5];
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({});
+
+    try {
+      await dispatch(
+        reviewsActions.addReviewToSpot(spotId, review, stars, sessionUser)
+      );
+      spotFetch();
+      closeModal();
+    } catch (res) {
+      const data = await res.json();
+      if (data.errors) {
+        setErrors(data.errors);
+      }
+    }
   };
 
   return (
-    <div className="review-form">
-      <h2 className="font">How was your stay?</h2>
-      <p className="err">{error}</p>
+    <form className="review-form" onSubmit={handleSubmit}>
+      <h1>How was your stay?</h1>
+      {Object.keys(errors).length !== 0 && (
+        <p>{`Errors: ${Object.keys(errors)}`}</p>
+      )}
       <textarea
-        className="textarea"
+        type="textarea"
+        className="input-review"
+        placeholder="Leave your review here..."
         value={review}
-        onChange={(e) => {
-          setError("");
-          setReview(e.target.value);
-        }}
-        required
-        placeholder="Leave your review here.."
+        onChange={(e) => setReview(e.target.value)}
       ></textarea>
-
-      <div className="star-rating">
-        <div className="star">
-          <input
-            type="radio"
-            id="star5"
-            name="rating"
-            value={stars}
-            onChange={() => setStars(5)}
-          />
-          <label htmlFor="star5" title="text">
-            5 stars
-          </label>
-          <input
-            type="radio"
-            name="rating"
-            id="star4"
-            value={stars}
-            onChange={() => setStars(4)}
-          />
-          <label htmlFor="star4" title="text">
-            4 stars
-          </label>
-          <input
-            type="radio"
-            name="rating"
-            id="star3"
-            value={stars}
-            onChange={() => setStars(3)}
-          />
-          <label htmlFor="star3" title="text">
-            3 stars
-          </label>
-          <input
-            type="radio"
-            id="star2"
-            name="rating"
-            value={stars}
-            onChange={() => setStars(2)}
-          />
-          <label htmlFor="star2" title="text">
-            2 stars
-          </label>
-          <input
-            type="radio"
-            id="star1"
-            name="rating"
-            value={stars}
-            onChange={() => setStars(1)}
-          />
-          <label htmlFor="star1" title="text">
-            1 star
-          </label>
-        </div>
-        Stars
+      <div className="star-container">
+        {fiveStars.map((star) => (
+          <i
+            key={star}
+            className={`fa-solid fa-star star ${
+              star <= (starCount || stars) ? "active" : ""
+            }`}
+            onMouseEnter={() => starHovering(star)}
+            onMouseLeave={() => starHovering(null)}
+            onClick={starSelect}
+          ></i>
+        ))}
+        <label>Stars</label>
       </div>
-
       <button
         className="review-submit"
         disabled={review.length < 10 || stars < 1}
@@ -105,7 +75,7 @@ const ReviewFormModal = ({ spotId, spotFetch }) => {
       >
         Submit Your Review
       </button>
-    </div>
+    </form>
   );
 };
 
