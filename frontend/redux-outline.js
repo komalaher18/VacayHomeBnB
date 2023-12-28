7a69797463d1de2e81fb73e587750602c7d4bc3a
Komal aher <komalaher18@gmail.com>
2023-12-27 22:51:22 -0800

    addes images & README

---
+++ frontend/redux-outline.js
@@ -0,0 +1,14 @@
+const airbnb = {
+    spots: {
+        id: {
+            // Spot data
+            name: 'Name of spot',
+            address: 'address',
+        },
+    },
+    reviews: {
+        id: {
+            // Review data
+        },
+    },
+};
